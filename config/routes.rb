c36ef3ee916b7c3dc5de67e191de8d549e4f6419
EC2 Default User <117354218+ayumu3221@users.noreply.github.com>
2022-12-15 07:19:24 +0000

    create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/edit'
+    get 'customers/show'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'customers/confirm'
+    get 'customers/edit'
+    get 'customers/show'
+  end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
