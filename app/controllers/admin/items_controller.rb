0c6738bf8b24340828a1c62ba36e7d84e13e3b56
EC2 Default User <ec2-user@ip-172-31-14-103.ap-northeast-1.compute.internal>
2022-12-19 09:24:10 +0000

    12/19

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @items = Item.all
+    @items = Item.all.page(params[:page]).per(10)
   end
 
   def new
@@ -14,33 +14,32 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
-    @item =Item.find(params[:id])
-    @genre = Genre.all
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
-  
+
   def create
    @item = Item.new(item_params)
    @genres = Genre.all
-    if @product.save
-      flash[:notice] = "You have created product successfully"
-      redirect_to admin_product_path(@product.id)
+
+    if @item.save
+      redirect_to admin_item_path(@item)
     else
       render :new
     end
   end
-  
+
   def update
    @item = Item.find(params[:id])
    @genres = Genre.all
    @item.update(item_params)
-   if @product.update(product_params)
-    flash[:notice] = "You have updated product successfully."
-    redirect_to admin_product_path(@product)
+   if @item.update(item_params)
+    redirect_to admin_item_path(@item)
    else
     render :edit
    end
   end
-  
+
   private
   def item_params
    params.require(:item).permit(:name, :body, :genre_id, :price, :image, :sale_status)
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,4 @@
 class Genre < ApplicationRecord
-  has_many :products, dependent: :destroy
+  has_many :items, dependent: :destroy
   validates :name, presence: true
 end
