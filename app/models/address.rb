73f83aa62d6e7dde326cbf71550e72f87f33e365
EC2 Default User <117354218+ayumu3221@users.noreply.github.com>
2022-12-21 04:24:31 +0000

    commit

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,9 +2,7 @@ class Address < ApplicationRecord
     
   belongs_to :customer
 
-  validates :postcode, presence: true
-  validates :address, presence: true
-  validates :name, presence: true
+  
 
   
 end
