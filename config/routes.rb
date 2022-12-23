8ad448596da59fb482735c8fe1b917a35bcce28f
Kakky <117184488+Kakky53@users.noreply.github.com>
2022-12-23 04:04:35 +0000

    view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,5 @@
 Rails.application.routes.draw do
-
-  namespace :admin do
-    resources :orders, only:[:index, :show, :update]
-    get "orders/current_user_order/:id" => "orders#current_user_order"
-    get 'homes/top'
-  end
-
-  root 'public/homes#top'
-  get 'about' => 'pubric/homes#about'
-
-  namespace :public do
-    resources :items, only: [:index, :show]
-    resources :cart_items, only: [:index, :update, :destroy, :create]
-  end
-
-  namespace :admin do
-    resources :items, only: [:index, :new, :create, :show, :edit, :update]
-    resources :genres, only: [:index, :edit, :create, :update]
-  end
-
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/edit'
-    get 'customers/show'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'customers/confirm'
-    get 'customers/edit'
-    get 'customers/show'
-  end
-
   
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/finish'
-    get 'orders/index'
-    get 'orders/show'
-  end
-
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
